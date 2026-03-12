c2e47e544d6fb5026946b1d77a42cb19053db2da
Jepchumbar <staceyjepchumbarr@gmail.com>
2026-03-12 18:23:13 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let student_name = 'Kibichii'
+let firstName = 'Emmanuel';
+let secondName = 'Kibichi';
+console.log(student_name, firstName, secondName)
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate)
+    
